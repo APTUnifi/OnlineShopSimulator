9a251287a5e45db7f9b58cccef9878ea4a5caf01
andreagemelli <gemelli.andrea@hotmail.it>
2019-11-23 12:02:30 +0100

    Handle SQL database with Spring Boot framework
    https://github.com/APTUnifi/OnlineShopSimulator/issues/12

--- OnlineShopSimulator/src/it/java/com/online/shop/repository/sql/ItemsSqlRepositoryIT.java
+++ OnlineShopSimulator/src/it/java/com/online/shop/repository/sql/ItemsSqlRepositoryIT.java
@@ -1,30 +1,39 @@
 package com.online.shop.repository.sql;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals; 
 
 import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public class ItemsSqlRepositoryIT {
 	
 	@ClassRule
-	public static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer();
+	public static final PostgreSQLContainer repo = new PostgreSQLContainer();
 	
 	@Before
 	public void setup() {
 		// setting up SQLDatabase
-		Flyway flyway = Flyway.configure().dataSource(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(),
-				postgreSQLContainer.getPassword()).load();
+		Flyway flyway = Flyway.configure().dataSource(
+				repo.getJdbcUrl(), 
+				repo.getUsername(),
+				repo.getPassword()).load();
+		
 		// TODO drop method to clean database
 		flyway.migrate();
 	}
 	
 	private ItemsSqlRepository buildRepository() {
-		return new ItemsSqlRepository(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(),
-				postgreSQLContainer.getPassword());
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUrl(repo.getJdbcUrl());
+		dataSource.setUsername(repo.getUsername());
+		dataSource.setPassword(repo.getPassword());
+		
+		return new ItemsSqlRepository(dataSource);
 	}
 	
 	@Test
--- OnlineShopSimulator/src/main/java/com/online/shop/repository/sql/ItemsSqlRepository.java
+++ OnlineShopSimulator/src/main/java/com/online/shop/repository/sql/ItemsSqlRepository.java
@@ -1,70 +1,21 @@
 package com.online.shop.repository.sql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ItemsSqlRepository {
 	
-	private final String jdbcUrl;
-	private final String username;
-	private final String password;
+	private JdbcTemplate jdbcTemplate;
 	
-	private Logger logger;
-	
-	public ItemsSqlRepository(String jdbcUrl, String username, String password) {
-		this.jdbcUrl = jdbcUrl;
-		this.username = username;
-		this.password = password;		
+	public ItemsSqlRepository(DataSource dataSource) {
 		
-		logger = Logger.getAnonymousLogger();
+		jdbcTemplate = new JdbcTemplate(dataSource);
+
 	}
 	
-	public long count() {	
-		
-		long numItems = 0;
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-			
-			conn = DriverManager.getConnection(jdbcUrl, username, password);
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("SELECT COUNT (*) FROM items");
-			rs.next();
-			numItems = rs.getInt(1);
-			
-		} catch (SQLException e) {
-			
-			logger.log(Level.SEVERE,"SQLException", e);
-			
-		} finally {
-			
-			try {
-				
-				if (rs != null && stmt != null && conn != null) {
-					rs.close();
-					stmt.close();
-					conn.close();
-				}
-				
-			} catch (NullPointerException e) {
-				
-				logger.log(Level.SEVERE,"NullPointerException", e);
-				
-			} catch (SQLException e) {
-				
-				logger.log(Level.SEVERE,"SQLException", e);
-				
-			}
-			
-		}
-		
-		return numItems;
+	public int count() {
+		return (int) jdbcTemplate.queryForObject("SELECT COUNT (*) FROM items", Integer.class);
 	}
+	
 }
